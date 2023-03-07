cb817b4950c524aa0f0d67c673052afb2bc37b10
bmarteau <bmarteau@amd.com>
2023-03-07 16:43:23 +0100

    removed the skipped floats tests from the linmod test suite.
    Change-Id: I684c2165d93341721d6ae42c3b6bd010ee15e79b

--- tests/gtests/linmod_tests.cpp
+++ tests/gtests/linmod_tests.cpp
@@ -7,18 +7,15 @@ typedef struct {
     std::string test_name;
     std::string data_name;
     linmod_model mod;
-    /* This should be options */
     std::vector<option_t<bool>> bopts;
     std::vector<option_t<std::string>> sopts;
     std::vector<option_t<float>> fopts;
     std::vector<option_t<double>> dopts;
     std::vector<option_t<int>> iopts;
-    // Some tests cannot run to sufficient accuracy in float
-    bool skip_float = false;
 } linmodParamType;
 
-const linmodParamType linmodPosValues[] = {
-    {"trivialMSENoint", "trivial", linmod_model_mse, {}, {}, {}, {}, {}, false},
+const linmodParamType linmodPosValuesD[] = {
+    {"trivialMSENoint", "trivial", linmod_model_mse, {}, {}, {}, {}, {}},
     {"trivialMSEI",
      "trivial",
      linmod_model_mse,
@@ -26,8 +23,7 @@ const linmodParamType linmodPosValues[] = {
      {},
      {},
      {},
-     {},
-     false},
+     {}},
     {"studyLogI",
      "study",
      linmod_model_logistic,
@@ -35,9 +31,8 @@ const linmodParamType linmodPosValues[] = {
      {},
      {},
      {},
-     {},
-     true},
-    {"studyLogNoint", "study", linmod_model_logistic, {}, {}, {}, {}, {}, true},
+     {}},
+    {"studyLogNoint", "study", linmod_model_logistic, {}, {}, {}, {}, {}},
     {"lrsetLogI",
      "lrset",
      linmod_model_logistic,
@@ -45,12 +40,13 @@ const linmodParamType linmodPosValues[] = {
      {},
      {},
      {},
-     {},
-     true},
-    {"lrsetLogNoint", "lrset", linmod_model_logistic, {}, {}, {}, {}, {}, true}};
+     {}},
+    {"lrsetLogNoint", "lrset", linmod_model_logistic, {}, {}, {}, {}, {}}};
+const linmodParamType linmodPosValuesF[] = {linmodPosValuesD[0], linmodPosValuesD[1]};
 
 // Data Driven (parametrized) Tests
-class linmodTestPos : public testing::TestWithParam<linmodParamType> {};
+class linmodTestPosD : public testing::TestWithParam<linmodParamType> {};
+class linmodTestPosF : public testing::TestWithParam<linmodParamType> {};
 
 // Teach GTest how to print the param type
 // in this case use only user's unique testname
@@ -58,7 +54,7 @@ class linmodTestPos : public testing::TestWithParam<linmodParamType> {};
 void PrintTo(const linmodParamType &param, ::std::ostream *os) { *os << param.test_name; }
 
 // Positive (da_status_success) tests with double type
-TEST_P(linmodTestPos, Double) {
+TEST_P(linmodTestPosD, Double) {
     // Inside a test, access the test parameter with the GetParam() method
     // of the TestWithParam<T> class:
     const linmodParamType &param = GetParam();
@@ -66,17 +62,16 @@ TEST_P(linmodTestPos, Double) {
                                  param.dopts, param.iopts);
 }
 // Positive (da_status_success) tests with float type
-TEST_P(linmodTestPos, Float) {
+TEST_P(linmodTestPosF, Float) {
     // Inside a test, access the test parameter with the GetParam() method
     // of the TestWithParam<T> class:
     const linmodParamType &param = GetParam();
-    if (param.skip_float)
-        GTEST_SKIP() << "Single precision is not enough to achieve good enough accuracy";
-
     test_linmod_positive<float>(param.data_name, param.mod, param.bopts, param.sopts,
                                 param.fopts, param.iopts);
 }
 
-INSTANTIATE_TEST_SUITE_P(linmodPosSuite, linmodTestPos,
-                         testing::ValuesIn(linmodPosValues));
+INSTANTIATE_TEST_SUITE_P(linmodPosSuiteD, linmodTestPosD,
+                         testing::ValuesIn(linmodPosValuesD));
+INSTANTIATE_TEST_SUITE_P(linmodPosSuiteF, linmodTestPosF,
+                         testing::ValuesIn(linmodPosValuesF));
 } // namespace
