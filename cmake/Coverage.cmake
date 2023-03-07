a14987a5095b657ccbfa9d2b5a7b7b37e8e71076
bmarteau <bmarteau@amd.com>
2023-03-07 11:13:48 +0100

    coverage target now always rebuilds the project before creating the reports.
    Change-Id: I728aafe3564a02878f1cdb045920bceb882089b7

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -7,23 +7,22 @@ link_libraries(gcov)
 
 # create a coverage_reports directory in the main build dir
 set(COV_DIR ${CMAKE_BINARY_DIR}/coverage_reports)
-add_custom_target(create_cov_dir
-COMMAND ${CMAKE_COMMAND} -E make_directory
-                                             ${COV_DIR})
+add_custom_target(create_cov_dir COMMAND ${CMAKE_COMMAND} -E make_directory
+                                         ${COV_DIR})
 
-# create coverage target for ctest
-# make coverage will run ctest and build a report in the build directory
+# create coverage target for ctest make coverage will run ctest and build a
+# report in the build directory
 add_custom_target(
   coverage
   DEPENDS create_cov_dir
+  COMMAND ${CMAKE_MAKE_PROGRAM} -C ${CMAKE_CURRENT_BINARY_DIR} all
   COMMAND ctest > /dev/null
   COMMAND ${GCOVR_PATH} --html ${COV_DIR}/index.html --html-details -r
           ${CMAKE_SOURCE_DIR} -e .*_deps*. .*/build/.*)
 
-# function to build a coverage report from a set of executable names
-#      argument: string containing a list of all executables to run
-# - runs all the executables in ARGN
-# - generate an htm gcovr report in ${COV_DIR
+# function to build a coverage report from a set of executable names argument:
+# string containing a list of all executables to run - runs all the executables
+# in ARGN - generate an htm gcovr report in ${COV_DIR
 function(exec_cov_target)
   set(REPORT_DEP "")
   foreach(EXEC ${ARGN})
