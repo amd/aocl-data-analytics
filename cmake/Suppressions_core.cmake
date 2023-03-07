8a463081d80f2c90248040361642b819fdd0514f
bmarteau <bmarteau@amd.com>
2023-03-07 12:41:31 +0100

    separated the suppressions flags between c and c++ files to remove the compiler warning.
    Change-Id: Icc5babbb8fc5318b886f83fed397aec59c1b89f6

--- cmake/Suppressions_core.cmake
+++ cmake/Suppressions_core.cmake
@@ -1,6 +1,13 @@
 # csv
 set_source_files_properties(
-  core/csv/read_csv_public.cpp core/csv/read_csv.cpp core/csv/tokenizer.c core/utilities/da_handle_public.cpp
+  core/csv/read_csv_public.cpp core/csv/read_csv.cpp
+  core/utilities/da_handle_public.cpp
+  PROPERTIES
+    COMPILE_FLAGS
+    "-Wno-unused-variable -Wno-unused-parameter -Wno-implicit-fallthrough -Wno-format -Wno-sign-compare -Wno-type-limits"
+)
+set_source_files_properties(
+  core/csv/tokenizer.c
   PROPERTIES
     COMPILE_FLAGS
     "-Wno-unused-variable -Wno-unused-parameter -Wno-implicit-fallthrough -Wno-format -Wno-old-style-declaration -Wno-sign-compare -Wno-type-limits"
