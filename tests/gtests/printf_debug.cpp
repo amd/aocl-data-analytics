b7e2a0dc0200735e860e132f21cf12a845105254
Philip Maybank <pmaybank@amd.com>
2023-07-05 11:27:06 +0100

    Make DA_PRINTF_DEBUG variadic
    - Use __VA_OPT__ and __VA_ARGS__ in definition of DA_PRINTF_DEBUG
      so that we can call macro with string only, and with multiple
      arguments after format string.
    - Also define DA_INT_FMT so that we can eliminate warnings in 64 bit
      integer builds that are generated as a result of using incorrect
      integer format.
    - Update test to include use cases described above.
    
    Change-Id: Ia6e85e61017c51a998f1df3cc3d3df5afa8d365c

--- tests/gtests/printf_debug.cpp
+++ tests/gtests/printf_debug.cpp
@@ -5,14 +5,27 @@ int main()
 {
     std::cout << "----------------------------------------" << std::endl;
     std::cout << "Unit test for DA_PRINTF_DEBUG" << std::endl;
-    std::cout << "If DA_LOGGING is defined output should be" << std::endl;;
-    std::cout << "[main printf_debug.cpp:15] x =     2.90" << std::endl;
+    std::cout << "If DA_LOGGING is defined output should be" << std::endl;
+    std::cout << std::endl;
+    std::cout << "[main printf_debug.cpp:24] Inside main " << std::endl;
+    std::cout << "[main printf_debug.cpp:26] x =   2.90 " << std::endl;
+    std::cout << "[main printf_debug.cpp:28] x =   2.90, y = 15 " << std::endl;
+    std::cout << std::endl;
     std::cout << "Otherwise DA_PRINTF_DEBUG should be silent" << std::endl;
     std::cout << "----------------------------------------" << std::endl;
+    std::cout << std::endl;
 
     double x = 1.45;
     x = x * 2;
-    DA_PRINTF_DEBUG("x = %8.2f\n", x);
+    da_int y = 12;
+    y = y + 3;
+
+    // check macro works when we are only printing a string with no variables
+    DA_PRINTF_DEBUG("Inside main \n");
+    // check macro works when we are printing a single variable
+    DA_PRINTF_DEBUG("x = %6.2f\n", x);
+    // check macro works when we are printing multiple variables including a variable of integer type
+    DA_PRINTF_DEBUG("x = %6.2f, y = %" DA_INT_FMT "\n", x, y);
 
     return 0;
 }
