428ec6782a9fd8e186b0d03f59626ba96aeb01b9
bmarteau <bmarteau@amd.com>
2023-08-24 15:37:52 +0200

    Fixing race condition in parallel build
    Change-Id: Ib7a13478aa348ccf16a4d81b01cb4fea4c19a15f

--- external/Lbfgsb.3.0/sworking_precision.F90
+++
@@ -1,3 +0,0 @@
-Module sworking_precision
-    Integer, parameter :: wp = selected_real_kind(6)
-End Module
---
+++ external/Lbfgsb.3.0/working_precision.F90
@@ -0,0 +1,4 @@
+Module working_precision
+    Integer, parameter :: dp = selected_real_kind(15)
+    Integer, parameter :: sp = selected_real_kind(6)
+End Module
