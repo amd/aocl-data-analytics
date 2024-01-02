c7e8c94da85edeaa17d50ac785074c7ee34c0207
Andrew Sajo <andrew.sajo@amd.com>
2024-01-02 15:09:23 +0000

    Inherit math style font from HTML theme
    MathJax can "inherit" the font typeface
    specified in the HTML theme used in the
    \text{} or \mbox{} blocks, rather than render it
    using the standard LaTeX font typeface.
    
    Change-Id: I91d234d8dea0fd46185b82eda50d6aa70df19999
    Reviewed-on: https://gerrit-git.amd.com/c/cpulibraries/er/aocl-da/+/982266
    Reviewed-by: Benjamin Marteau <Benjamin.Marteau@amd.com>
    Tested-by: Osi Request <Osi.Request@amd.com>
    Tested-by: Llvm Cpu <llvm.cpu@amd.com>

--- doc/conf.py
+++ doc/conf.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (C) 2023-2024 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
@@ -33,18 +33,25 @@ author = 'Advanced Micro Devices, Inc'
 version = ''
 release = '4.2.1'
 
-
 # -- General configuration ---------------------------------------------------
 extensions = ['sphinxcontrib.bibtex', 'breathe', 'sphinx_collapse']
 bibtex_bibfiles = ['refs.bib']
 bibtex_reference_style = 'author_year'
 breathe_default_project = 'aocl-da'
 
-# -- Option for generating output conditional on cmake INTERNAL_DOC variable --
+# -- Option for generating output conditional on cmake INTERNAL_DOC variable -
 exclude_patterns = ['**/doc/trees_forests/df_intro.rst']
 
-# Add any paths that contain templates here, relative to this directory.
+# -- Add any paths that contain templates here, relative to this directory. --
 templates_path = ['_template']
+
+# -- MathJax config ----------------------------------------------------------
+mathjax3_config = {
+    'chtml' : {
+        'mtextInheritFont' : 'true',
+    }
+}
+
 
 # -- Options for HTML output -------------------------------------------------
 
