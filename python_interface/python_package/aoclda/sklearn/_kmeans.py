18ba8c09c530ec3f810e94f8f65748e03d6e3af2
Edvin Hopkins <edvin.hopkins@amd.com>
2024-04-24 11:08:52 +0100

    Return scalars not size 1 numpy arrays for sklearn compatibility
    Change-Id: Ic31259f7e6237e003c75d1a2b20a0cb26f7e32fc
    Reviewed-on: https://gerrit-git.amd.com/c/cpulibraries/er/aocl-da/+/1043759
    Tested-by: Osi Request <Osi.Request@amd.com>
    Tested-by: Llvm Cpu <llvm.cpu@amd.com>
    Reviewed-by: Edvin Hopkins <Edvin.Hopkins@amd.com>

--- python_interface/python_package/aoclda/sklearn/_kmeans.py
+++ python_interface/python_package/aoclda/sklearn/_kmeans.py
@@ -163,11 +163,11 @@ class kmeans(kmeans_sklearn):
 
     @property
     def inertia_(self):
-        return self.kmeans.get_inertia()
+        return self.kmeans.get_inertia()[0]
 
     @property
     def n_iter_(self):
-        return self.kmeans.get_n_iter()
+        return self.kmeans.get_n_iter()[0]
 
     @property
     def n_features_in_(self):
