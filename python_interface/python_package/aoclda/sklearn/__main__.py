0315d957aa2b1da134dcb05d47c887d9a14fa019
bmarteau <bmarteau@amd.com>
2024-12-18 17:34:00 +0100

    Split the sklearn patch init into __init__ and __main__
    __main__ is necessary to not get an error when running a script through python -m aoclda.sklearn
    Also adding an option to main to not print the patch message
    
    Change-Id: Ie42f53ad64a69dd763a455a7d50864148fd742f3
    Reviewed-on: http://gerrit-git.amd.com/c/cpulibraries/er/aocl-da/+/1170981
    Tested-by: Osi Request <Osi.Request@amd.com>
    Tested-by: Llvm Cpu <llvm.cpu@amd.com>
    Reviewed-by: Edvin Hopkins <Edvin.Hopkins@amd.com>

--- python_interface/python_package/aoclda/sklearn/__main__.py
+++ python_interface/python_package/aoclda/sklearn/__main__.py
@@ -23,10 +23,41 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 
-
-# pylint: disable = missing-module-docstring, import-outside-toplevel
+# pylint: disable = missing-module-docstring, import-outside-toplevel, no-member
 
 import sys
-from .patch_sklearn import skpatch, undo_skpatch
+from aoclda.sklearn import skpatch
 
-__all__ = ["skpatch", "undo_skpatch"]
+def main():
+    '''
+    Load the scikit-learn patch then execute the user's script
+    '''
+
+    import argparse
+    import runpy
+
+    parser = argparse.ArgumentParser(
+        description="AOCL-DA Extension for scikit-learn")
+
+    parser.add_argument(
+        "-m", action="store_true", dest="is_module")
+    parser.add_argument("name", help="Your Python script or module name")
+    parser.add_argument("args", nargs=argparse.REMAINDER,
+                        help="Command line arguments for your Python script")
+    parser.add_argument("--print-patch", default=True, dest="print_patched",
+                        help="Print welcome message", action=argparse.BooleanOptionalAction)
+
+    args = parser.parse_args()
+
+    # Call patch to replace scikit-learn symbols with AOCL-DA
+    skpatch(print_patched=args.print_patched)
+
+    sys.argv = [args.name] + args.args
+
+    if args.is_module:
+        runpy.run_module(args.name, run_name="__main__")
+    else:
+        runpy.run_path(args.name, run_name="__main__")
+
+if __name__ == "__main__":
+    sys.exit(main())
